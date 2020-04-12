c3a20854fb08af8dfb7ac8797bc1b741088329a7
NicatAqayev <nicat.aqayev@gmail.com>
2020-04-12 13:39:06 +0400

    nicat.task

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// 1. bir selectbox olur ve yaninda bir button. selectboxdan bir option secdikde. buttona click olunduqda sechilmish deyeri console-da gosterin.
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         console.log($("#selectbox").val());
+//     })
+// })
+
+// ---------------------------------//
+
+
+
+// 3. bir check olunmamish checkbox inputunuz olsun ve altinda button olsun. eger checkbox check olunmayibsa button disable olsun ve check olunubsa ise enable olsun.
+
+// $(document).ready(function(){
+//     var checkBox = $("#chbox");
+//     if(checkBox.checked == true){
+//         $("button").css("display", "block");
+//     }
+//     else{
+//         $("button").css("display", "none");
+//     }
+// });
+
+// function myFunction() {
+//     var chbox=document.querySelector("#chbox");
+//     var buton = document.querySelector(".btn");
+
+//     if(chbox.checked == true){
+//         buton.style.display = "block";
+//     }else{
+//         buton.style.display = "none";
+//     }
+// }
+//  myFunction();
+
+// ---------------------------------//
